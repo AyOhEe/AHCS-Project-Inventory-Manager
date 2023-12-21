e055ddf01bbe7b084039c66a5b58fd9177f492df
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-12-21 19:54:20 +0000

    Moved literals into constants
    Again, code clarity

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ from website import Website
 WINDOW_UPDATE_DELAY = 0.1
 
 
+#directory containing the website page templates
+TEMPLATES_PATH = "Jinja templates/"
+#address at which the website hosts itself
+HOST_ADDRESS = "localhost"
+#port which the website hosts itself
+PORT = 8080
+
+
 async def main():
     #create the dashboard window
     window = Dashboard()
     #create the website server
-    app = Website("Jinja templates/")
+    app = Website(TEMPLATES_PATH)
 
     #start the website in the background
-    await app.start_website("localhost", 8080)
+    await app.start_website(HOST_ADDRESS, PORT)
 
     #start the dashboard window's loop. 
     #tk.Tk.mainloop is not used as it blocks the main thread, 
