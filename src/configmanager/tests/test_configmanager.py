7bdaa352c232b6692e01bfb73a1560aff2bd502c
AyOhEe <awesomepantsap@gmail.com>
2024-02-22 13:27:46 +0000

    Outlined test_configmanager.py

--- src/configmanager/tests/test_configmanager.py
+++ src/configmanager/tests/test_configmanager.py
@@ -5,4 +5,19 @@ from configmanager import ConfigManager
 
 
 class TestConfigManager(unittest.TestCase):
-    pass
+    @unittest.expectedFailure
+    def test_0_initialise(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_1_get_config_value(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_2_set_config_value(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_3_save_config(self):
+        self.fail("Unimplemented test")
+        
