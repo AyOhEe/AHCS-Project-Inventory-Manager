0b66d33d6d467dd3d3b32ffe0bdcb0386386f6a7
AyOhEe <awesomepantsap@gmail.com>
2024-02-22 13:25:16 +0000

    Outlined test_listingmanager.py

--- src/listingmanager/tests/test_listingmanager.py
+++ src/listingmanager/tests/test_listingmanager.py
@@ -5,4 +5,44 @@ from listingmanager import ListingManager
 
 
 class TestListingManager(unittest.TestCase):
-    pass
+    @unittest.expectedFailure
+    def test_0_initialise(self):
+        self.fail("Unimplemented test")
+
+    @unittest.expectedFailure
+    def test_1_create_listing(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_2_update_listing(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_3_remove_listing(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_4_get_listing_index(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_5_add_stock(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_6_remove_stock(self):
+        self.fail("Unimplemented test")
+        
+
+    @unittest.expectedFailure
+    def test_7_get_all_listings(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_8_get_listing(self):
+        self.fail("Unimplemented test")
+        
+    @unittest.expectedFailure
+    def test_9_query_listings(self):
+        self.fail("Unimplemented test")
+        
